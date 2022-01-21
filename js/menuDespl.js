e98a8b42afa513e196ed98ffaec9fd3418a50e43
alfredo-mendi <82898136+alfredo-mendi@users.noreply.github.com>
2022-01-21 22:33:41 +0100

    V1.0 Funciona casi todo en versión bilingüe.

--- js/menuDespl.js
+++ js/menuDespl.js
@@ -5,7 +5,7 @@ function cambiaIdioma(idioma) {
     };
     /* alert(id1);
     alert(id2);*/
-    var colEs = document.getElementsByClassName('es'); /* Hay el mismo número de es que de cn */
+    var colEs = document.getElementsByClassName('es'); /* Hay el mismo número de es que de cn, no hace falta contar los 'cn' */
     for (var i = 0, len = colEs.length; i < len; i++) {
         document.getElementsByClassName(id1)[i].style.display = "block";
         document.getElementsByClassName(id2)[i].style.display = "none";
@@ -14,4 +14,9 @@ function cambiaIdioma(idioma) {
     for (var i = 0, len = colPie.length; i < len; i++) {
         document.getElementsByClassName("pie-de-pagina " + id1)[i].style.display = "flex";
     }*/
+}
+function cambiaPagina(pagina) {
+    for (i=0;i<4;i++){
+    document.getElementById(i).style.display = "none";}
+    document.getElementById(pagina).style.display="block";
 }
